f28dc6526820842678b759575f17d3b207003c39
mowoka <giomowoka@gmail.com>
2021-02-06 21:08:38 +0700

    add Login-APi

--- src/controllers/registerLogin.js
+++ src/controllers/registerLogin.js
@@ -70,6 +70,72 @@ exports.register = async (req, res) => {
   }
 };
 
+exports.login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const schema = Joi.object({
+      email: Joi.string().email().min(6).required(),
+      password: Joi.string().min(8).required(),
+    });
+
+    const { error } = schema.validate({
+      email: req.body.email,
+      password: req.body.password,
+    });
+
+    if (error) {
+      return res.status(400).send({
+        message: error.details[0].message,
+      });
+    }
+
+    const user = await User.findOne({
+      where: {
+        email,
+      },
+    });
+
+    if (!user) {
+      return res.status(400).send({
+        message: "Your Credential is not valid",
+      });
+    }
+
+    const validPass = await bcrypt.compare(password, user.password);
+
+    if (!validPass) {
+      return res.status(400).send({
+        message: "Your Credential is not valid",
+      });
+    }
+
+    const secretKey = "Mokaz-Dev";
+    const token = jwt.sign(
+      {
+        id: user.id,
+      },
+      secretKey
+    );
+
+    res.send({
+      status: "Success",
+      message: "Login Success",
+      data: {
+        chanel: {
+          email,
+          token,
+        },
+      },
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).send({
+      message: "Server Error",
+    });
+  }
+};
+
 // exports.dummy = async (req, res) => {
 //   try {
 //   } catch (err) {
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { register } = require("../controllers/registerLogin");
+const { register, login } = require("../controllers/registerLogin");
 
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
